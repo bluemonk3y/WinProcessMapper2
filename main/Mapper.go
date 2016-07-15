62cfb8a99b7fce6d9530c47f76130d4fcbb5a312
bluemonk3y <neil.avery@excelian.com>
2016-07-15 12:55:34 +0100

    added web tier

--- main/Mapper.go
+++ main/Mapper.go
@@ -8,18 +8,33 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"github.com/influxdata/influxdb/client/v2"
+	"time"
+	"os"
+	"net"
 )
 
-func main() {
+const (
+	MyDB = "introspector-1"
+	username = ""
+	password = ""
+)
+type ServerStats struct {
+	hostname, ip_public, ip_address, ip_subnet, ip_gateway string
+	processes, file_handles, server_ports, client_ports int
+	cpu_load, disk_load, net_load, mem_util float32
 
+}
+
+
+func processHandles(stats *ServerStats, processMap map[string]map[string][]string) {
 	handle := exec.Command("./etc/Handle.exe")
 	handlesPipe,err := handle.StdoutPipe()
 	handle.Start()
 	handlesProcessReader := bufio.NewReader(handlesPipe)
 
-
-	var processMap = make(map[string]map[string][]string)
 	var currentPIdMap = make(map[string][]string)
+
 
 	for {
 		var line string
@@ -36,17 +51,20 @@ func main() {
 			panic(err)
 		}
 
-//		fmt.Print(line)
+		//fmt.Print(line)
 		if (strings.Contains(line, "pid:")) {
 
-			var parts = strings.Fields(line)
-			currentPIdMap = make(map[string][]string)
+			if (!strings.Contains(line, "unable to open process")) {
 
-			currentPIdMap["name"] = []string { parts[0] }
-			currentPIdMap["pid"] = []string { parts[2] }
-			currentPIdMap["owner"] = []string { parts[3] }
-			currentPIdMap["files"] = []string{}
-			processMap["pid"] = currentPIdMap
+				var parts = strings.Fields(line)
+				currentPIdMap = make(map[string][]string)
+
+				currentPIdMap["name"] = []string{parts[0] }
+				currentPIdMap["pid"] = []string{parts[2] }
+				currentPIdMap["owner"] = []string{parts[3] }
+				currentPIdMap["files"] = []string{}
+				processMap[parts[2]] = currentPIdMap
+			}
 
 		} else if (strings.Contains(line, " File ")){
 			var parts = strings.Fields(line)
@@ -54,14 +72,18 @@ func main() {
 			var fileIndex = strings.LastIndex(line, sofile)
 			var ff = line[fileIndex:len(line)]
 			currentPIdMap["files"] = append(currentPIdMap["files"],ff)
+			stats.file_handles += 1
 		}
 	}
 
 
 	// Wait for the result of the command; also closes our end of the pipe
 	err = handle.Wait()
-	//fmt.Printf("HANDLES %s\n", handles)
+	fmt.Printf("HANDLES 2 %s\n", len(processMap))
 
+}
+
+func processNetstat(stats *ServerStats, processMap map[string]map[string][]string) {
 	netout := exec.Command("netstat", "-a", "-n", "-o")
 	netstatPipe,err2 := netout.StdoutPipe()
 
@@ -81,14 +103,128 @@ func main() {
 		if err2 != nil {
 			panic(err2)
 		}
+		if (strings.Contains(line, "LISTENING")) {
+			stats.server_ports += 1
+		}
+		if (strings.Contains(line, "ESTABLISHED")) {
+			stats.client_ports += 1
+		}
 
 		fmt.Print(line)
-	 }
+	}
+}
 
 
 
-	if err2 != nil {
+
+func writeServerStats(serverStats *ServerStats)  {
+
+
+	/**
+	* InfluxDB connection
+	*
+	 */
+
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		//		Addr: "http://localhost:8086",
+		Addr: "http://192.168.99.100:32768",
+		Username: username,
+		Password: password,
+	})
+
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  MyDB,
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+
+	tags := map[string]string{ "server": serverStats.hostname, "ip_address": serverStats.ip_address, "ip_subnet": serverStats.ip_subnet, "ip_gateway" : serverStats.ip_gateway}
+	fields := map[string]interface{}{
+		"processes": serverStats.processes,
+		"cpu_load":  serverStats.cpu_load,
+		"disk_load":   serverStats.disk_load,
+		"net_load":   serverStats.net_load,
+		"mem_util":   serverStats.mem_util,
+		"ports_server": serverStats.server_ports,
+		"ports_client":  serverStats.client_ports,
+		"file_handles": serverStats.file_handles,
+		"ip_address": serverStats.ip_address,
+		"ip_public": serverStats.ip_public,
+		"ip_gateway": serverStats.ip_gateway,
+		"ip_subnet": serverStats.ip_subnet,
+	}
+	// + subnet, ip
+	pt, err := client.NewPoint("server_stats", tags, fields, time.Now())
+
+
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+
+	bp.AddPoint(pt)
+
+	// Write the batch
+	c.Write(bp)
+}
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
 		log.Fatal(err)
 	}
-//	fmt.Printf("NETSTAT %s\n", netout)
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().String()
+	idx := strings.LastIndex(localAddr, ":")
+
+	return localAddr[0:idx]
+}
+
+
+func main() {
+
+
+	stats := new(ServerStats)
+
+	var processMap = make(map[string]map[string][]string)
+
+
+	fmt.Printf("handles ")
+	processHandles(stats, processMap)
+
+
+	fmt.Printf("netstat ")
+
+	processNetstat(stats, processMap)
+
+
+
+	hhh, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	stats.processes = len(processMap)
+	stats.hostname = hhh
+	stats.net_load = 0.12
+	stats.disk_load = 0.5
+	stats.cpu_load = 25.5
+	stats.ip_address = GetOutboundIP()
+
+	writeServerStats(stats)
+
+
+
+//	if err2 != nil {
+//		log.Fatal(err)
+//	}
+	fmt.Printf("output host:%s handles:%d\n", stats.hostname, stats.file_handles)
 }
---
+++ static/js/index.js
@@ -0,0 +1,6 @@
+
+// without jQuery (doesn't work in older IEs)
+document.addEventListener('DOMContentLoaded', function(){
+    // your code goes here
+    console.log("booooooooooom!")
+}, false);
